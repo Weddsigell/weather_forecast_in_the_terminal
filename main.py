8c67bf4783cd7f64e861affaef6be82d6e3b86e0
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2024-12-14 16:48:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import requests
 
 
 def main():
-    city = "Пермь"
-    url_template = 'https://wttr.in/{}?nMmqT&lang=ru'
-    url = url_template.format(city)
-    response = requests.get(url)
-    response.raise_for_status()
-    print(response.text)
+    cities= ["Лондон", "Череповец", "SVO"]
+    params = {"n": "", "M": "", "m": "", "q": "", "T": "", "lang": "ru" }
+    url_template = 'https://wttr.in/{}'
+    for city in cities:
+        url = url_template.format(city)
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        print(response.text)
 
 
 if __name__ == '__main__':
